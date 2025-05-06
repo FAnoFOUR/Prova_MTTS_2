78a4fe29d19f3f6b06bb112d574644a224f16013
34Eli <elisa.marchioro04@gmail.com>
2025-05-06 21:14:18 +0200

    close #7 Creata funzione IntegerToRoman

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////
+// [ELISA] [MARCHIORO] [2111941]
+// [ANTONIO] [TANG] [2111017]
+////////////////////////////////////////////////////////////////////
+
+
+
+package it.unipd.mtss;
+
+
+public class IntegerToRoman {
+  public static String convert(int number){
+    String roman = "";
+    String[] romanletters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int[] integers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+    if (number < 1 || number > 1000) {
+      throw new IllegalArgumentException("Numero non valido");
+    }
+    for (i = 0; i <= 12; i++) {
+    while (number >= integers[i]){
+      roman.append(romanletters[i]);
+      number = number - integers[i];
+    }
+    }
+    return roman;
+  }
+}
